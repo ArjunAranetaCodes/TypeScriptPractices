bbba938eba966a55928ea3822f3b8aea9d50b262
MoreCodes <arjunaraneta@gmail.com>
2023-10-10 16:30:47 +0800

    TypeScript Exercises

---
+++ 01-Simple-Types/01-string.ts
@@ -0,0 +1,4 @@
+let firstName: string = "Arjun"; // type string
+
+console.log(firstName);
+console.log(typeof firstName);
---
+++ commit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add .
+git commit --date "2023-10-10" -m "TypeScript Exercises"
+git push origin
